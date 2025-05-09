32a095ea1c4e0da66e5b9c92efbf9c9484984acf
howardon951 <wasd5123@gmail.com>
2025-05-09 20:27:17 +0800

    build config for github pages

--- odtab-website/next.config.ts
+++ odtab-website/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+// for github pages
+const repo = "online-drum-tab";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/drum-sequencer" : "",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     unoptimized: true,
   },
