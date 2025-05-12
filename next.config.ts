b45e672c29ab583381bd3ea4249966a99c362727
howardon951 <wasd5123@gmail.com>
2025-05-12 15:22:09 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const assetPrefix = process.env.AssetPrefix ?? "";
+
 const nextConfig: NextConfig = {
+  assetPrefix: assetPrefix,
   output: "export",
   reactStrictMode: true,
   images: {
